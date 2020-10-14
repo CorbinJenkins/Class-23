5b2512daa3f8046af9b2944054f6eda84439bba0
CorbinJenkins <71853790+CorbinJenkins@users.noreply.github.com>
2020-10-14 16:59:20 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var eng, world;
+var ground;
+var box1,box2;
+
+function setup() {
+  createCanvas(400,400);
+
+  eng = Engine.create();
+  //console.log(eng);
+
+  world = eng.world;
+
+  ground=new Ground(200,380,400,20);
+
+  box1=new Box(200,300,50,100);
+  box2=new Box(240,100,50,50);
+
+}
+
+function draw() {
+  background(0);  
+
+  Engine.update(eng);
+  
+  box1.display();
+  box2.display();
+  ground.display();
+
+  
+  
+  
+}
